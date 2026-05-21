9b7a349c707e275c7eda6e667fa745fc4a5dab27
Brenda-moreira25 <brenda_rssl@hotmail.com>
2026-05-21 19:50:44 -0300

    first commit

---
+++ nome/nome.model.js
@@ -0,0 +1,13 @@
+export class FilaModel {
+  constructor() {
+    this.fila = [];
+  }
+
+  getTesouro() {
+    return this.tesouro;
+  }
+
+  setTesouro(tesouro) {
+    this.tesouro = tesouro;
+  }
+}
